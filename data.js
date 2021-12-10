985ee89ca08f756f5df694041f8b934ce3f644ab
hsinyeeteh <95460311+hsinyeeteh@users.noreply.github.com>
2021-12-10 11:08:55 +0800

    first commit
    This is the first commit

---
+++ data.js
@@ -0,0 +1,124 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.33268567900475077,
+        "pitch": -0.12827030355812674,
+        "fov": 1.2344983363832869
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3701246617345131,
+          "pitch": -0.053589566511266895,
+          "rotation": 6.283185307179586,
+          "target": "2-engineering-auditorium"
+        },
+        {
+          "yaw": -1.9171937983972125,
+          "pitch": -0.08786224540108023,
+          "rotation": 0,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7854215970609086,
+          "pitch": -0.19408609014953448,
+          "title": "Kent Vale",
+          "text": "Staff hostel"
+        }
+      ]
+    }
+  ],
+  "name": "Faculty of Engineering",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
